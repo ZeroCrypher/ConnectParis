f72e42469e18e3ad89eab2bec81133153292b863
Saïd Benzerara <benzerarasaid2@gmail.com>
2025-02-11 20:35:57 +0100

    modification du css et du html de MainPage

--- src/Components/Main/MainPage.tsx
+++ src/Components/Main/MainPage.tsx
@@ -4,7 +4,26 @@ function MainPage() {
   return (
     <>
       <div className="reduc">Réduction de 15% avec le code JSPFRR</div>
-      <div className="info"></div>
+      <div className="info">
+        <span className="sp">
+          <a href="">
+            <h5><b>GET MESURED UP</b></h5>
+            <h6>Follow this link to get started.</h6>
+          </a>
+        </span>
+        <span className="sp">
+          <a href="">
+            <h5><b>THE PERFECT FIT</b></h5>
+            <h6>Completely made to measure - just for you.</h6>
+          </a>
+        </span>
+        <span className="sp">
+          <a href="">
+            <h5><b>CUSTOMIZABLE</b></h5>
+            <h6>Choose fabrics, linings, and everything in between.</h6>
+          </a>
+        </span>
+      </div>
       <div className="main">Tout ici :)</div>
     </>
   );
